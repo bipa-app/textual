1d5a1a888f93fff01332a1df9ab828ee54cd535c
Luiz Parreira <luizfilipester@gmail.com>
2026-01-30 19:22:57 -0300

    ios 16

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
   name: "textual",
   platforms: [
     .macOS(.v15),
-    .iOS(.v18),
+    .iOS(.v16),
     .tvOS(.v18),
     .watchOS(.v11),
-    .visionOS(.v2),
+    .visionOS(.v2)
   ],
   products: [
     .library(name: "Textual", targets: ["Textual"])
@@ -17,37 +17,37 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.3.1"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.7"),
-    .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
+    .package(url: "https://github.com/bipa-app/swiftui-math", branch: "ios16")
   ],
   targets: [
     .target(
       name: "Textual",
       dependencies: [
         .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
-        .product(name: "SwiftUIMath", package: "swiftui-math"),
+        .product(name: "SwiftUIMath", package: "swiftui-math")
       ],
       resources: [
         .process("Internal/Highlighter/Prism")
       ],
       swiftSettings: [
         .define("TEXTUAL_ENABLE_LINKS", .when(platforms: [.macOS, .iOS, .watchOS, .visionOS])),
-        .define("TEXTUAL_ENABLE_TEXT_SELECTION", .when(platforms: [.macOS, .iOS, .visionOS])),
+        .define("TEXTUAL_ENABLE_TEXT_SELECTION", .when(platforms: [.macOS, .iOS, .visionOS]))
       ]
     ),
     .testTarget(
       name: "TextualTests",
       dependencies: [
         "Textual",
-        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
       ],
       exclude: [
         "Internal/TextInteraction/__Snapshots__",
-        "StructuredText/__Snapshots__",
+        "StructuredText/__Snapshots__"
       ],
       resources: [.copy("Fixtures")],
       swiftSettings: [
         .define("TEXTUAL_ENABLE_TEXT_SELECTION", .when(platforms: [.macOS, .iOS, .visionOS]))
       ]
-    ),
+    )
   ]
 )
